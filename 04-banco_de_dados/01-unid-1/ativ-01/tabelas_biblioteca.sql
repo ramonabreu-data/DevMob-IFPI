47e3321c35a3f9a415e9b625d2d1ffa45dc8974e
ramonabreu-data <cdevabreu@gmail.com>
2023-09-11 19:09:54 -0300

    db-unidade1

--- 04-banco_de_dados/01-unid-1/ativ-01/tabelas_biblioteca.sql
+++ 04-banco_de_dados/01-unid-1/ativ-01/tabelas_biblioteca.sql
@@ -1,18 +1,20 @@
---tabela usuario
+
+/* criar tabelas*/
+
 CREATE TABLE Usuario (
-    ID INT PRIMARY KEY,
+    ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome VARCHAR(50),
     Email VARCHAR(100)
 );
---tabela livrp
+
 CREATE TABLE Livro (
-    ID INT PRIMARY KEY,
+    ID INT AUTO_INCREMENT PRIMARY KEY,
     Titulo VARCHAR(100),
     Autor VARCHAR(50)
 );
---Tabeela emprestimo
+
 CREATE TABLE Emprestimo (
-    ID INT PRIMARY KEY,
+    ID INT AUTO_INCREMENT PRIMARY KEY,
     ID_Usuario INT,
     ID_Livro INT,
     Data_Emprestimo DATE,
@@ -21,12 +23,44 @@ CREATE TABLE Emprestimo (
     FOREIGN KEY (ID_Livro) REFERENCES Livro(ID)
 );
 
---Consulta tabela Usuario
-SELECT * FROM Usuario;
+/* inserir  dados */
 
---consulta tabela Livro
-SELECT * FROM Livro;
+INSERT INTO Usuario (Nome, Email)
+VALUES
+  ('Ana Lima', 'ana@example.com'),
+  ('Pedro Santos', 'pedro@example.com'),
+  ('Mariana Oliveira', 'mariana@example.com'),
+  ('Carlos Silva', 'carlos@example.com'),
+  ('Luiza Fernandes', 'luiza@example.com');
+ 
+ 
+ INSERT INTO Livro (Titulo, Autor)
+VALUES
+  ('Aventuras Incríveis', 'Carlos Lima'),
+  ('O Mistério do Enigma', 'Ana Ferreira'),
+  ('O Tesouro Perdido', 'Paulo Rodrigues'),
+  ('Viagem ao Centro da Terra', 'Júlio Verne'),
+  ('Histórias de Fantasia', 'Maria Souza');
+ 
+ 
+ INSERT INTO Emprestimo (ID_Usuario, ID_Livro, Data_Emprestimo, Data_Devolucao)
+VALUES
+  (1, 1, '2023-09-01', '2023-09-08'),
+  (2, 2, '2023-09-02', '2023-09-09'),
+  (3, 3, '2023-09-03', '2023-09-10'),
+  (4, 4, '2023-09-04', '2023-09-11'),
+  (5, 5, '2023-09-05', '2023-09-12');
+
+ /* select tabela usuario */
+ 
+ SELECT * FROM Usuario 
+ 
+ /* select do id usuario , livro e datas */
+  
+SELECT e.ID, u.Nome AS NomeUsuario, l.Titulo AS TituloLivro, e.Data_Emprestimo, e.Data_Devolucao
+FROM Emprestimo e
+JOIN Usuario u ON e.ID_Usuario = u.ID
+JOIN Livro l ON e.ID_Livro = l.ID;
 
 
---consulta tabela Emprestimo
-SELECT * FROM Emprestimo;
+
